e9f45127e88a6a1e092df4c1cfde6a5a1557c95f
xdossantos <xola.dossantos@xdode.com>
2020-08-12 05:17:01 +0200

    Adding unversioned files

---
+++ blog-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bdf2d197699cc75422fe436e43aeda6e'),
+    },
+  },
+});
---
+++ blog-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5d1b85c8-1b79-44ea-bf38-bad0fefc1bf5'
+};
